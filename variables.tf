497e01d3bada8bc7bd9ff86f1be1f1f9231e60c8
gmorpheme <github@gmorpheme.net>
2016-01-04 10:55:19 +0000

    Abstract out a few variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,30 @@
 variable "vpc" {}
+
 variable "domain_name" {}
+
 variable "zone_id" {}
+
+variable "instance_type" {
+  description = "The EC2 instance type for ECS container instances"
+  default = "t2.nano"
+}
+
+variable "image_id" {
+  description = "The AMI to use for ECS container instances"
+  default = "ami-4fe4852a"
+}
+
+variable "cluster_min" {
+  description = "Minimum cluster size"
+  default = 1
+}
+
+variable "cluster_max" {
+  description = "Maximum cluster size"
+  default = 2
+}
+
+variable "cluster_desired_size" {
+  description = "Desired cluster size"
+  default = 2
+}
