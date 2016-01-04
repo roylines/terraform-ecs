6d105600649a3ff085db50f699302986f9f12db8
roylines <roy@roylines.co.uk>
2016-01-04 11:35:57 +0000

    added descriptions

--- variables.tf
+++ variables.tf
@@ -1,10 +1,19 @@
-variable "vpc" {}
+variable "vpc" {
+  description = "the desired vpc name"
+}
 
-variable "domain_name" {}
+variable "domain_name" {
+  description = "the domain name to configure routing to in route 53"
+}
 
-variable "zone_id" {}
+variable "zone_id" {
+  description = "the zone_id to use"
+}
 
-variable "region" {}
+variable "region" {
+  description = "the region"
+  default = "us-east-1"
+}
 
 variable "instance_type" {
   description = "The EC2 instance type for ECS container instances"
