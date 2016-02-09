0b97a2ff0c03cc55ab1cfb57e7392df4c11d98d0
roylines <roy@roylines.co.uk>
2016-02-09 15:37:59 +0000

    use health check url

--- api-gateway.tf
+++ api-gateway.tf
@@ -61,7 +61,7 @@ resource "aws_elb" "api_gateway" {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
-    target = "HTTP:8000/"
+    target = "HTTP:8000/api/v1/health"
     interval = 30
   }
   tags {
