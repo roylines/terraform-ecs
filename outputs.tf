96e0784a9db1c054354c668c15ab2b3e2507384c
roylines <roy@roylines.co.uk>
2016-04-01 16:23:17 +0100

    added some more outputs

--- outputs.tf
+++ outputs.tf
@@ -2,19 +2,19 @@ output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
 
-output "vpc_name" {
-  value = "${var.vpc}"
+output "cluster_id" {
+  value = "${aws_ecs_cluster.cluster.id}"
 }
 
-output "ecs_cluster_id" {
-  value = "${aws_ecs_cluster.cluster.id}"
+output "private_zone_id" {
+  value = "{aws_route53_zone.microservices.id}"
 }
 
 output "iam_role_arn" {
   value = "${aws_iam_role.server_role.arn}"
 }
 
-output "subnet_ids" {
+output "subnets" {
  value = "${join(",", aws_subnet.public.*.id)}"
 }
 
