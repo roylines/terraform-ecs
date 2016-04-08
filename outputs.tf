2117c85077be1b1633507a947943cd5926ab0473
roylines <roy@roylines.co.uk>
2016-04-08 09:27:05 +0100

    removed iam-service

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,6 @@ output "private_zone_id" {
   value = "${aws_route53_zone.microservices.zone_id}"
 }
 
-output "iam_role_arn" {
-  value = "${aws_iam_role.server_role.arn}"
-}
-
 output "subnets" {
  value = "${join(",", aws_subnet.public.*.id)}"
 }
